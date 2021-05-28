2c999505d7b14a54b34ea6106f0d340f8a07261e
midhunvnadh <midhun.vnadh5732@gmail.com>
2021-05-28 23:55:15 +0530

    Add goutham's work and add Home and Product folder/components for development

---
+++ src/components/Product/Product.js
@@ -0,0 +1,84 @@
+import "./main.scss";
+function Product() {
+  return (
+    <>
+      <header>
+        <h2>Covidbot</h2>
+      </header>
+      <section>
+        <nav>
+          <ul>
+            <li>
+              <a href="#">Thiruvananthapuram</a>
+            </li>
+            <li>
+              <a href="#"> Kollam</a>
+            </li>
+            <li>
+              <a href="#">Alappuzha</a>
+            </li>
+            <li>
+              <a href="#">Pathanamthitta</a>
+            </li>
+            <li>
+              <a href="#">Kottayam</a>
+            </li>
+            <li>
+              <a href="#">Idukki</a>
+            </li>
+            <li>
+              <a href="#">Ernakulam</a>
+            </li>
+            <li>
+              <a href="#">Thrissur</a>
+            </li>
+            <li>
+              <a href="#">Palakkad</a>
+            </li>
+            <li>
+              <a href="#">Malappuram</a>
+            </li>
+            <li>
+              <a href="#">Kozhikode</a>
+            </li>
+            <li>
+              <a href="#">Wayanadu</a>
+            </li>
+            <li>
+              <a href="#">Kannur</a>
+            </li>
+            <li>
+              <a href="#">Kasaragod</a>
+            </li>
+          </ul>
+        </nav>
+
+        <article>
+          <h1>What is covidbot?</h1>
+          <p>
+            Covidbot is app which we can install in our smart phones to get the
+            informtion about covid vaccination in ur location and also book
+            appoinments for vaccination. its is avilable in google playstore and
+            apple store and it is free to install.
+          </p>
+          <p>
+            As per the current pandamic situvation it essental to get vaccinated
+            saftly as per preregestering we can avoide crow and get safty
+            ensured. So get vaccinated and get save.
+          </p>
+        </article>
+      </section>
+
+      <footer>
+        <p>
+          <b>Covidbot</b> an health, socialservice platform from <b>Redsoc</b>
+        </p>
+        <p>
+          Terms of Service| Privacy| Statement| Master agreement| Copyright
+          2021| Redsoc All Rights Reserved|
+        </p>
+      </footer>
+    </>
+  );
+}
+export default Product;
