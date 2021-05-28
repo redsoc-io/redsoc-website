c9ed7f34af28baed391f86e884288a0cb00109e6
midhunvnadh <midhun.vnadh5732@gmail.com>
2021-05-28 23:54:32 +0530

    Setup routes to Product page and Home page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import { Switch, Route } from "react-router-dom";
+import Product from "./components/Product/Product";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          REDSOC
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Switch>
+      <Route path="/">
+        <Product />
+      </Route>
+      <Route path="/product"></Route>
+    </Switch>
   );
 }
 
