1294f9667e58de10e5f4a2ac60f27d7164eeb213
bilalapple247 <bilalapple247@gmail.com>
2022-04-30 23:24:21 +0530

    footer added-updated1

--- src/components/Home/Linksection.js
+++ src/components/Home/Linksection.js
@@ -2,22 +2,13 @@ import { Link } from "react-router-dom";
 function Ls() {
   return (
     <>
-      <section>
+      <div>
         <div>
           <div>
-            <div>
-              <Link to="/products"> PRODUCTS</Link>
-            </div>
+            <Link to="/products"> PRODUCTS</Link>
           </div>
         </div>
-      </section>
-
-      <footer>
-        <p>
-          Terms of Service| Privacy| Statement| Master agreement| Copyright
-          2022| Redsoc All Rights Reserved|
-        </p>
-      </footer>
+      </div>
     </>
   );
 }
