975284f5c707fcd3f7717e0d77288190d7ddf2b9
bilalapple247 <bilalapple247@gmail.com>
2022-04-30 23:16:31 +0530

    footer added-updated

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -3,11 +3,22 @@ import "./home.scss";
 import Members from "./Members";
 function Home() {
   return (
-    <section>
-      <div>
-        <Members />
-      </div>
-    </section>
+    <>
+      <section>
+        <div>
+          <Members />
+        </div>
+      </section>
+      <footer>
+        <p>
+          <b>Covidbot</b> an health, socialservice platform from <b>Redsoc</b>
+        </p>
+        <p>
+          Terms of Service| Privacy| Statement| Master agreement| Copyright
+          2021| Redsoc All Rights Reserved|
+        </p>
+      </footer>
+    </>
   );
 }
 export default Home;
