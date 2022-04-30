129baf8f0511205d1533a8c18e4f0793059eb514
bilalapple247 <bilalapple247@gmail.com>
2022-04-30 23:10:53 +0530

    footer added

--- src/components/Home/Linksection.js
+++ src/components/Home/Linksection.js
@@ -1,15 +1,24 @@
 import { Link } from "react-router-dom";
 function Ls() {
   return (
-    <section>
-      <div>
+    <>
+      <section>
         <div>
           <div>
-            <Link to="/products"> PRODUCTS</Link>
+            <div>
+              <Link to="/products"> PRODUCTS</Link>
+            </div>
           </div>
         </div>
-      </div>
-    </section>
+      </section>
+
+      <footer>
+        <p>
+          Terms of Service| Privacy| Statement| Master agreement| Copyright
+          2022| Redsoc All Rights Reserved|
+        </p>
+      </footer>
+    </>
   );
 }
 export default Ls;
