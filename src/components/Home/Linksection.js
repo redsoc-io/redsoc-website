0543e3dc96e66020f7ed3c8b51783e256833235c
bilalapple247 <bilalapple247@gmail.com>
2022-05-01 17:07:06 +0530

    Navigation bar home page

--- src/components/Home/Linksection.js
+++ src/components/Home/Linksection.js
@@ -5,7 +5,15 @@ function Ls() {
       <div>
         <div>
           <div>
-            <Link to="/products"> PRODUCTS</Link>
+            <div class="topnav">
+              <a class="active" href="/">
+                Home
+              </a>
+              <a href="/">News</a>
+              <a href="/">Contact</a>
+              <a href="/">About</a>
+              <a href="/products">Products</a>
+            </div>
           </div>
         </div>
       </div>
